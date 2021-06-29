c6317dc4581ac2656e68ccdd85a971da91f8abc7
Lynne2021 <86622973+Lynne2021@users.noreply.github.com>
2021-06-28 22:12:32 -0400

    Added allo

--- src/pkgDemo/Main.java
+++ src/pkgDemo/Main.java
@@ -3,7 +3,7 @@ package pkgDemo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Allo");
+        System.out.println("A");
 	// write your code here
     }
 }
